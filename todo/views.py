8eec38b5a9b4e3d2cadff4be9aa4b6872731cb26
xianfuxing <xianfuxing@126.com>
2016-10-19 11:42:33 +0800

    # Add Django 1.10 support

--- todo/views.py
+++ todo/views.py
@@ -1,22 +1,17 @@
-# -*-coding: utf-8 -*-
-#!/usr/bin/env python
-from __future__ import unicode_literals
-
-from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.http import Http404
-from models import Todo
+from .models import Todo
 
 
 def todolist(request):
     todolist = Todo.objects.filter(flag=1)
     finishtodos = Todo.objects.filter(flag=0)
-    return render_to_response('simpleTodo.html',
-        {'todolist': todolist, 'finishtodos': finishtodos},
-        context_instance=RequestContext(request))
-
+    return render(request, 'todo/simpleTodo.html',
+                          {'todolist': todolist,
+                           'finishtodos': finishtodos})
 
 def todofinish(request, id=''):
     todo = Todo.objects.get(id=id)
@@ -25,9 +20,8 @@ def todofinish(request, id=''):
         todo.save()
         return HttpResponseRedirect('/todos/')
     todolist = Todo.objects.filter(flag=1)
-    return render_to_response('simpleTodo.html', {'todolist': todolist},
-        context_instance=RequestContext(request))
-
+    return render(request, 'todo/simpleTodo.html',
+                           {'todolist': todolist})
 
 def todoback(request, id=''):
     todo = Todo.objects.get(id=id)
@@ -36,9 +30,7 @@ def todoback(request, id=''):
         todo.save()
         return HttpResponseRedirect('/todos/')
     todolist = Todo.objects.filter(flag=1)
-    return render_to_response('simpleTodo.html', {'todolist': todolist},
-        context_instance=RequestContext(request))
-
+    return render(request, 'todo/simpleTodo.html', {'todolist': todolist})
 
 def tododelete(request, id=''):
     try:
@@ -49,9 +41,7 @@ def tododelete(request, id=''):
         todo.delete()
         return HttpResponseRedirect('/todos/')
     todolist = Todo.objects.filter(flag=1)
-    return render_to_response('simpleTodo.html', {'todolist': todolist},
-        context_instance=RequestContext(request))
-
+    return render(reqeust, 'todo/simpleTodo.html', {'todolist': todolist})
 
 def addTodo(request):
     if request.method == 'POST':
@@ -62,15 +52,15 @@ def addTodo(request):
         todo.save()
         todolist = Todo.objects.filter(flag='1')
         finishtodos = Todo.objects.filter(flag=0)
-        return render_to_response('showtodo.html',
-            {'todolist': todolist, 'finishtodos': finishtodos},
-            context_instance=RequestContext(request))
+        return render(request, 'todo/showtodo.html',
+                              {'todolist': todolist, 
+                               'finishtodos': finishtodos})
     else:
         todolist = Todo.objects.filter(flag=1)
         finishtodos = Todo.objects.filter(flag=0)
-        return render_to_response('simpleTodo.html',
-            {'todolist': todolist, 'finishtodos': finishtodos})
-
+        return render(request, 'todo/simpleTodo.html',
+                              {'todolist': todolist, 
+                               'finishtodos': finishtodos})
 
 def updatetodo(request, id=''):
     if request.method == 'POST':
@@ -89,5 +79,5 @@ def updatetodo(request, id=''):
             todo = Todo.objects.get(id=id)
         except Exception:
             raise Http404
-        return render_to_response('updatatodo.html', {'todo': todo},
-            context_instance=RequestContext(request))
+        return render(request, 'todo/updatetodo.html', {'todo': todo})
+
